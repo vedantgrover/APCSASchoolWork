ee3bdd12bbf52d3fd4db7dea0db4b0b1bf9dc1dc
bladedurman <vedantvgrover@gmail.com>
2022-03-01 20:07:41 -0800

    Edit for ArraySupplement.
    Added userInput Compatibility for ArithmeticGeometricMean

--- Projects/ArrayListSummativeSupplement/ArithmeticGeometricMean.java
+++ Projects/ArrayListSummativeSupplement/ArithmeticGeometricMean.java
@@ -1,17 +1,32 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArithmeticGeometricMean {
     public static void main(String[] args) {
-        ArrayList<Double> arrList = new ArrayList<Double>();
+        while (true) {
+            ArrayList<Double> arrList = new ArrayList<Double>();
 
-        arrList.add(4.0);
-        arrList.add(7.0);
-        arrList.add(7.0);
-        arrList.add(1.0);
-        arrList.add(9.0);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println(arithmeticMean(arrList));
-        System.out.println(geometricMean(arrList));
+            System.out.print("Please enter in your numbers.\nYou can ([q]uit) too.\nMake sure to seperate the numbers with commans! Max : 10\n>> ");
+            String userResponse = sc.nextLine();
+
+            String[] numbers = (userResponse.contains(", ")) ? userResponse.split(", ") : userResponse.split(",");
+
+            if (numbers[0].toLowerCase().contains("q")) break;
+
+            if (numbers.length > 10) {
+                System.out.println("Too many numbers bro");
+                continue;
+            }
+
+            for (String num : numbers) {
+                arrList.add(Double.parseDouble(num));
+            }
+
+            System.out.println(arithmeticMean(arrList));
+            System.out.println(geometricMean(arrList));
+        }
     }
 
     public static double arithmeticMean(ArrayList<Double> arrList) {
@@ -29,6 +44,6 @@ public class ArithmeticGeometricMean {
             product *= num;
         }
 
-        return Math.pow(product, 1.0/arryList.size());
+        return Math.pow(product, 1.0 / arryList.size());
     }
 }
